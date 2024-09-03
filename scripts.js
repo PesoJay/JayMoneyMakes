8f5757cad1e5a6d3161552a3324db95cc74563a6
jakobdanid01 <jakob.deschauer@gmx.com>
2024-09-03 20:44:10 +0200

    fade in animation for info

--- scripts.js
+++ scripts.js
@@ -3,7 +3,10 @@ var titleCardName = 'Jakob Daniel Deschauer';
 var speed = 50;
 
 function start(){
+    console.log("hallo");
     typeTitleCardName();
+    checkFadeInElements(); // Initial check for elements in view
+    window.addEventListener('scroll', checkFadeInElements);
 }
 
 function typeTitleCardName(){
@@ -12,4 +15,23 @@ function typeTitleCardName(){
         i++;
         setTimeout(typeTitleCardName, speed);
     }
+}
+
+function checkFadeInElements() {
+    const fadeInElements = document.querySelectorAll('.fade-in-section');
+    fadeInElements.forEach(element => {
+        if (isElementInViewport(element)) {
+            element.classList.add('is-visible');
+        }
+    });
+}
+
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
 }
